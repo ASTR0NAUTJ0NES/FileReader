c2d657c18bfa26af8d378b08f905d00bf81ec359
Andrew-P-Albanese <36201692+ASTR0NAUTJ0NES@users.noreply.github.com>
2018-11-20 17:30:13 -0500

    Initial commit

---
+++ Lab11bTest.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Lab11bTest
+{
+    public static void main(String[] args) throws IOException 
+    {
+        FileReader fileReaderObj = new FileReader("start.txt");
+    }
+}
